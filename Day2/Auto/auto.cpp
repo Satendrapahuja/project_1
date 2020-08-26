650f138d79cea26f6473cf57ee10876249bfab98
Satendra Pahuja <69447953+Satendrapahuja@users.noreply.github.com>
2020-08-26 11:49:47 +0530

    Add files via upload

--- Day2/Auto/auto.cpp
+++ Day2/Auto/auto.cpp
@@ -1,1 +1,20 @@
-
+#include<iostream>
+float* flt() {
+	float f{ 10.52f };
+	return &f;
+}
+int main() {
+	auto a = 10;
+	auto d = 20.5;
+	auto r = 'M';
+	decltype(flt()) f;
+	std::cout << "a is :- "<<a <<std::endl;
+	std::cout << "Size of a is :-  "<< sizeof(a) <<std::endl;
+	std::cout << "d is :- " << d << std::endl;
+	std::cout << "Size of d is :-  " << sizeof(d) << std::endl;
+	std::cout << "r is :- " << r << std::endl;
+	std::cout << "Size of r is :-  " << sizeof(r) << std::endl;
+	std::cout << "f is :- " << typeid(f)<< std::endl;
+	std::cout << "Size of flt is :-  " << sizeof(f) << std::endl;
+	return 0;
+}
